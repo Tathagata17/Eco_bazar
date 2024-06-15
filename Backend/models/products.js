d913e0e7bed8d17f95528f8693975ff5f1ed1425
Tathagata17 <tathagata14n14@gmail.com>
2024-06-15 23:18:44 +0530

    first frontend

---
+++ Backend/models/products.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    stock: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    images: [{
+        url: {
+            type: String,
+            required: true
+        },
+        alt: {
+            type: String,
+            required: true
+        }
+    }],
+    brand: {
+        type: String,
+        trim: true
+    },
+    ratings: {
+        averageRating: {
+            type: Number,
+            default: 0,
+            min: 0,
+            max: 5
+        },
+        reviewsCount: {
+            type: Number,
+            default: 0,
+            min: 0
+        }
+    },
+    reviews: [{
+        user: {
+           type:String,
+           required:true,
+        },
+        rating: {
+            type: Number,
+            required: true,
+            min: 0,
+            max: 5
+        },
+        comment: {
+            type: String,
+            trim: true
+        },
+    }],
+});
+
+const productmodel=mongoose.model("products",productSchema);
+
+module.exports=productmodel;
